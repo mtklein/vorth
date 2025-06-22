1c19e5dbfe0939faf3653563b127fe216c40828e
Mike Klein <mtklein@gmail.com>
2025-06-22 09:46:09 +0800

    Add vector implementations for vorth ops (#2)
    * Implement vector operations
    
    * refine vorth vector ops
    
    * Implement vector ops with F16/F32 types
    
    * move vector helpers to implementation
    
    * Fix tests to use scalar stacks
    
    * Restore header and update tests
    
    * Simplify constants and drop pragmas

--- vorth.c
+++ vorth.c
@@ -1,1 +1,102 @@
 #include "vorth.h"
+
+#define V 8
+
+typedef _Float16 F16 __attribute__((vector_size(sizeof(_Float16) * V)));
+typedef float    F32 __attribute__((vector_size(sizeof(float) * V)));
+
+#define splat(T, x) ((T){0} + 1) * (x)
+
+void* vorth_imm_f16(void* sp, _Float16 x) {
+    F16* stack = sp;
+    *stack++ = splat(F16, x);
+    return stack;
+}
+
+void* vorth_add_f16(void* sp) {
+    F16* stack = sp;
+    F16 b = *--stack;
+    F16 a = *--stack;
+    *stack++ = a + b;
+    return stack;
+}
+
+void* vorth_sub_f16(void* sp) {
+    F16* stack = sp;
+    F16 b = *--stack;
+    F16 a = *--stack;
+    *stack++ = a - b;
+    return stack;
+}
+
+void* vorth_mul_f16(void* sp) {
+    F16* stack = sp;
+    F16 b = *--stack;
+    F16 a = *--stack;
+    *stack++ = a * b;
+    return stack;
+}
+
+void* vorth_div_f16(void* sp) {
+    F16* stack = sp;
+    F16 b = *--stack;
+    F16 a = *--stack;
+    *stack++ = a / b;
+    return stack;
+}
+
+void* vorth_mad_f16(void* sp) {
+    F16* stack = sp;
+    F16 c = *--stack;
+    F16 b = *--stack;
+    F16 a = *--stack;
+    *stack++ = a * b + c;
+    return stack;
+}
+
+void* vorth_imm_f32(void* sp, float x) {
+    F32* stack = sp;
+    *stack++ = splat(F32, x);
+    return stack;
+}
+
+void* vorth_add_f32(void* sp) {
+    F32* stack = sp;
+    F32 b = *--stack;
+    F32 a = *--stack;
+    *stack++ = a + b;
+    return stack;
+}
+
+void* vorth_sub_f32(void* sp) {
+    F32* stack = sp;
+    F32 b = *--stack;
+    F32 a = *--stack;
+    *stack++ = a - b;
+    return stack;
+}
+
+void* vorth_mul_f32(void* sp) {
+    F32* stack = sp;
+    F32 b = *--stack;
+    F32 a = *--stack;
+    *stack++ = a * b;
+    return stack;
+}
+
+void* vorth_div_f32(void* sp) {
+    F32* stack = sp;
+    F32 b = *--stack;
+    F32 a = *--stack;
+    *stack++ = a / b;
+    return stack;
+}
+
+void* vorth_mad_f32(void* sp) {
+    F32* stack = sp;
+    F32 c = *--stack;
+    F32 b = *--stack;
+    F32 a = *--stack;
+    *stack++ = a * b + c;
+    return stack;
+}
