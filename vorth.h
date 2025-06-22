13d8872c06c36c8a7bd3847542ed9e9716e3c49a
Mike Klein <mtklein@gmail.com>
2025-06-22 10:31:10 +0800

    Add SIMD integer operations (#7)
    * Refine integer API
    
    * Refactor SIMD ops with macros

--- vorth.h
+++ vorth.h
@@ -2,20 +2,6 @@
 
 #define V 8
 
-// vorth is a SIMT-style vectorized Forth-style language.  Its priorities:
-//   - correctness
-//   - efficient memory access
-//   - optimized execution
-//   - compact code size
-
-// vorth methods take and return the stack pointer as first parameter.
-// Suffixes indicate data size and type, e.g.
-//   _f16 -> 16-bit float
-//   _f32 -> 32-bit float
-//   _s8  -> signed byte
-//   _u16 -> unsigned short
-//   _i32 -> bitwise or integer operations where signedness doesn't matter.
-
 void* vorth_imm_f16(void*, _Float16);
 void* vorth_add_f16(void*);
 void* vorth_sub_f16(void*);
@@ -32,3 +18,48 @@ void* vorth_mad_f32(void*);
 
 void* vorth_pop_f16(void*, _Float16[V]);
 void* vorth_pop_f32(void*, float[V]);
+
+void* vorth_imm_s8(void*, signed char);
+void* vorth_pop_s8(void*, signed char[V]);
+
+void* vorth_imm_s16(void*, short);
+void* vorth_pop_s16(void*, short[V]);
+
+void* vorth_imm_s32(void*, int);
+void* vorth_pop_s32(void*, int[V]);
+
+void* vorth_imm_u8(void*, unsigned char);
+void* vorth_pop_u8(void*, unsigned char[V]);
+
+void* vorth_imm_u16(void*, unsigned short);
+void* vorth_pop_u16(void*, unsigned short[V]);
+
+void* vorth_imm_u32(void*, unsigned int);
+void* vorth_pop_u32(void*, unsigned int[V]);
+
+void* vorth_add_i8(void*);
+void* vorth_sub_i8(void*);
+void* vorth_mul_i8(void*);
+
+void* vorth_add_i16(void*);
+void* vorth_sub_i16(void*);
+void* vorth_mul_i16(void*);
+
+void* vorth_add_i32(void*);
+void* vorth_sub_i32(void*);
+void* vorth_mul_i32(void*);
+
+void* vorth_and_i8(void*);
+void* vorth_or_i8(void*);
+void* vorth_xor_i8(void*);
+void* vorth_not_i8(void*);
+
+void* vorth_and_i16(void*);
+void* vorth_or_i16(void*);
+void* vorth_xor_i16(void*);
+void* vorth_not_i16(void*);
+
+void* vorth_and_i32(void*);
+void* vorth_or_i32(void*);
+void* vorth_xor_i32(void*);
+void* vorth_not_i32(void*);
--- vorth_test.c
+++ vorth_test.c
@@ -2,6 +2,7 @@
 #include <assert.h>
 
 
+
 static inline int equiv_f16(_Float16 x, _Float16 y) {
     return (x <= y && y <= x) || (x != x && y != y);
 }
@@ -9,6 +10,65 @@ static inline int equiv_f16(_Float16 x, _Float16 y) {
 static inline int equiv_f32(float x, float y) {
     return (x <= y && y <= x) || (x != x && y != y);
 }
+
+#define DEFINE_TEST_INT(name, stype, utype, suf, imm_s, pop_s, imm_u, pop_u) \
+static void name(void) {                                                    \
+    stype stack[8 * V] __attribute__((aligned(sizeof(stype) * V)));         \
+    void* sp = stack;                                                       \
+    sp = imm_s(sp, 1);                                                      \
+    sp = imm_s(sp, 2);                                                      \
+    sp = vorth_add_##suf(sp);                                               \
+    stype sres[V];                                                          \
+    sp = pop_s(sp, sres);                                                   \
+    for (int i = 0; i < V; i++)                                             \
+        assert(sres[i] == 3);                                              \
+    sp = stack;                                                             \
+    sp = imm_s(sp, 5);                                                      \
+    sp = imm_s(sp, 3);                                                      \
+    sp = vorth_sub_##suf(sp);                                               \
+    sp = pop_s(sp, sres);                                                   \
+    for (int i = 0; i < V; i++)                                             \
+        assert(sres[i] == 2);                                              \
+    sp = stack;                                                             \
+    sp = imm_s(sp, 2);                                                      \
+    sp = imm_s(sp, 3);                                                      \
+    sp = vorth_mul_##suf(sp);                                               \
+    sp = pop_s(sp, sres);                                                   \
+    for (int i = 0; i < V; i++)                                             \
+        assert(sres[i] == 6);                                              \
+    utype ures[V];                                                          \
+    sp = stack;                                                             \
+    sp = imm_u(sp, 0xaa);                                                   \
+    sp = imm_u(sp, 0xcc);                                                   \
+    sp = vorth_and_##suf(sp);                                               \
+    sp = pop_u(sp, ures);                                                   \
+    for (int i = 0; i < V; i++)                                             \
+        assert(ures[i] == (utype)(0xaa & 0xcc));                            \
+    sp = stack;                                                             \
+    sp = imm_u(sp, 0xaa);                                                   \
+    sp = imm_u(sp, 0xcc);                                                   \
+    sp = vorth_or_##suf(sp);                                                \
+    sp = pop_u(sp, ures);                                                   \
+    for (int i = 0; i < V; i++)                                             \
+        assert(ures[i] == (utype)(0xaa | 0xcc));                            \
+    sp = stack;                                                             \
+    sp = imm_u(sp, 0xaa);                                                   \
+    sp = imm_u(sp, 0xcc);                                                   \
+    sp = vorth_xor_##suf(sp);                                               \
+    sp = pop_u(sp, ures);                                                   \
+    for (int i = 0; i < V; i++)                                             \
+        assert(ures[i] == (utype)(0xaa ^ 0xcc));                            \
+    sp = stack;                                                             \
+    sp = imm_u(sp, 0xaa);                                                   \
+    sp = vorth_not_##suf(sp);                                               \
+    sp = pop_u(sp, ures);                                                   \
+    for (int i = 0; i < V; i++)                                             \
+        assert(ures[i] == (utype)(~0xaa));                                  \
+}
+
+DEFINE_TEST_INT(test_i8,  signed char,      unsigned char,      i8,  vorth_imm_s8,  vorth_pop_s8,  vorth_imm_u8,  vorth_pop_u8)
+DEFINE_TEST_INT(test_i16, short,            unsigned short,     i16, vorth_imm_s16, vorth_pop_s16, vorth_imm_u16, vorth_pop_u16)
+DEFINE_TEST_INT(test_i32, int,              unsigned int,       i32, vorth_imm_s32, vorth_pop_s32, vorth_imm_u32, vorth_pop_u32)
 
 static void test_f16(void) {
     _Float16 stack[8 * V] __attribute__((aligned(sizeof(_Float16) * V)));
@@ -113,6 +173,9 @@ static void test_f32(void) {
 }
 
 int main(void) {
+    test_i8();
+    test_i16();
+    test_i32();
     test_f16();
     test_f32();
     return 0;
